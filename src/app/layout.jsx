895435d48d5bada4e8796d5140a5952bb76c3839
djnovin <hello@novinnoori.com>
2022-11-06 08:02:01 +1100

    Setupt BoilerPlate for Context

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import "tailwindcss/tailwind.css"
 import AnalyticsWrapper from './Analytics';
  
@@ -6,28 +8,34 @@ import NavLink from "../components/NavLink"
 import NavBar from "../components/NavBar";
 import Banner from "../components/Banner"
 
+import { useState, createContext } from "react"
+import ThemeContext from "../context/ThemeContext"
 
 const RootLayout = ({ children }) => {
+  const [theme, setTheme] = useState('light');
     return (
         <html lang="en">
           <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Next.js</title>
           </head>
-          <body className="dark:bg-black">
-            <div className="">
-            <section>
-              <Banner />
-            </section>
-            <header className="flex flex-row w-full justify-between items-center align-middle">
-              <NavBar />
-            </header>
-            <section className="">
-                <div className="">{children}</div>
-                <AnalyticsWrapper />
-            </section>
-            </div>
-            </body>
+              <body className="dark:bg-black">
+                <ThemeContext.Provider value={{theme}}>
+                  <div className="">
+                  <section>
+                    <Banner />
+                  </section>
+                  <header className="flex flex-row w-full justify-between items-center align-middle">
+                    <NavBar />
+                  </header>
+                  <section className="">
+                      <div className="">{children}</div>
+                  </section>
+                  </div>
+                  <AnalyticsWrapper />
+                </ThemeContext.Provider>
+                </body>
+
         </html>
       );
     }    
