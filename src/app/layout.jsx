4efe1aefc8aa49cdb6e8c61921a411f0091d2d10
djnovin <hello@novinnoori.com>
2024-03-04 12:52:37 +1100

    Clean up Legacy Project

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,37 +1,15 @@
-"use client"
-import { ThemeProvider } from "next-themes";
-
-import "tailwindcss/tailwind.css"
-import AnalyticsWrapper from './Analytics';
- 
-import NavBar from "../components/NavBar";
-import Banner from "../components/Banner"
+import AnalyticsWrapper from './Analytics'
+import { Analytics } from '@vercel/analytics/react'
 
 const RootLayout = ({ children }) => {
     return (
-        <html lang="en">
-          <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Next.js</title>
-          </head>
-              <body className="dark:bg-black">
-                <ThemeProvider>
-                  <div className="">
-                  <section>
-                    <Banner />
-                  </section>
-                  <header className="flex flex-row w-full justify-between items-center align-middle">
-                    <NavBar />
-                  </header>
-                  <section className="">
-                      <div className="">{children}</div>
-                  </section>
-                  </div>
-                  <AnalyticsWrapper />
-                </ThemeProvider>
-              </body>
+        <html lang='en'>
+            <body>
+                <main className=''>{children}</main>
+                <Analytics />
+            </body>
         </html>
-      );
-    }    
+    )
+}
 
 export default RootLayout
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  darkMode: 'class',
-
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {}
+    },
+    darkMode: 'class'
 }
