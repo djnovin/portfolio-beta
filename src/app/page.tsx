5d46f917521b9463423b142efff2367ac6986408
djnovin <hello@novinnoori.com>
2024-06-11 09:54:44 +0930

    Update Bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,21 @@ const Page = () => {
         <div className='h-screen flex justify-center items-center px-8'>
             <h1 className='uppercase'>
                 I am a Software Designer / Engineer based in Sydney, Australia.
-                I am currently working at Akcelo as a Frontend Engineer.
             </h1>
+            <h1>Tech Stack</h1>
+            <ul>
+                <li>Rust</li>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>Node.js</li>
+                <li>GraphQL</li>
+                <li>PostgreSQL</li>
+                <li>Redis</li>
+                <li>Typescript</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+            </ul>
         </div>
     )
 }
