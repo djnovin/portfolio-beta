01181e780d02b9e3edf845baba6236110d685043
djnovin <hello@novinnoori.com>
2024-06-12 07:28:37 +0930

    Added Form with Action

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -179,12 +179,22 @@ export async function generateMetadata(
 export default function page({ params }: { params: { slug: string } }) {
     const blog = getBlog(params.slug)
 
+    const handleSubscribe = async (formData: FormData) => {
+        'use server'
+
+        const email = formData.get('email')
+        const subscribe = formData.get('subscribe')
+
+        if (email && subscribe) {
+            // send email to backend
+        }
+    }
+
     return (
-        <div className='px-8'>
+        <div className='px-8 pb-20'>
             {blog && (
                 <Breadcrumbs
                     crumbs={[
-                        { label: 'Home', path: '/' },
                         { label: 'Blog', path: '/blog' },
                         { label: blog.title, path: `/blog/${blog.slug}` }
                     ]}
@@ -192,6 +202,60 @@ export default function page({ params }: { params: { slug: string } }) {
                 />
             )}
             <RemoteMdxPage slug={params.slug} />
+            <form
+                className='border border-solid border-black p-4 mt-8 flex flex-col'
+                action={handleSubscribe}
+            >
+                <h2
+                    className='text-2xl font-bold mb-4'
+                    aria-label='Subscribe to newsletter title'
+                    role='heading'
+                >
+                    Subscribe
+                </h2>
+
+                <p
+                    className='mb-4'
+                    aria-label='Subscribe to newsletter description'
+                    role='description'
+                >
+                    If you enjoyed this article, consider subscribing to my
+                    newsletter. I'll send you an email when I publish a new
+                    article.
+                </p>
+                <label
+                    className='mb-2'
+                    htmlFor='email'
+                    aria-label='Email label'
+                    role='label'
+                >
+                    Email
+                </label>
+                <input
+                    aria-label='Email input'
+                    className='mb-4 rounded-none border border-solid border-black py-2 px-4'
+                    name='email'
+                    placeholder='Email'
+                    role='textbox'
+                    type='email'
+                />
+                <div className='flex flex-row gap-x-2'>
+                    <input
+                        aria-label='Subscribe to newsletter checkbox'
+                        id='subscribe'
+                        name='subscribe'
+                        role='checkbox'
+                        type='checkbox'
+                    />
+                    <label htmlFor='subscribe'>Subscribe to newsletter</label>
+                </div>
+                <button
+                    className='bg-black text-white py-2 px-4 mt-4 rounded-none border border-solid border-black'
+                    type='submit'
+                >
+                    Subscribe
+                </button>
+            </form>
         </div>
     )
 }
